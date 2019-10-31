45cb80e545d65084ad06cb3e7e59e1865bb40f84
arnecdn <root@workspacea6qbn1uuttz01f20.python-69f4c77b4d-xnkt5>
2019-10-31 06:10:59 +0000

    Working to solve challenge 13 at Ringzer0

---
+++ ch13.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+RINGZER0_URL="https://ringzer0ctf.com/login"
+LOGIN_FORM="\"username=arnecdn&password=test124\""
+LOGIN_HTTP_CONTENTTYPE=""
+LOGIN=$(curl -s -w -c cookie-jar.txt $RINGZER0_URL/login)
+LOGIN_POST="curl -b cookie-jar.txt -d $LOGIN_FORM -X POST $RINGZER0_URL/login"
+LOGIN_POST=${LOGIN_POST##*<form role=\"form\" action=\"\" method=\"post\">}
+LOGIN_POST=${LOGIN_POST%%</form>*}
+echo $LOGIN_POST
+#echo $response | sed -n '/<body>/,/<\/body>/{ /body>/d; p }'
+#sed -n ''
