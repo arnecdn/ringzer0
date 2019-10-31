f3a64bb27cfcfa7dc5b617dd6d14c663456d1d29
arnecdn <root@workspacea6qbn1uuttz01f20.python-69f4c77b4d-xnkt5>
2019-10-31 06:21:00 +0000

    Adjusting curl command to use cookies correctly

--- ch13.sh
+++ ch13.sh
@@ -2,8 +2,8 @@
 RINGZER0_URL="https://ringzer0ctf.com/login"
 LOGIN_FORM="\"username=arnecdn&password=test124\""
 LOGIN_HTTP_CONTENTTYPE=""
-LOGIN=$(curl -s -w -c cookie-jar.txt $RINGZER0_URL/login)
-LOGIN_POST="curl -b cookie-jar.txt -d $LOGIN_FORM -X POST $RINGZER0_URL/login"
+LOGIN=$(curl -s --cookie-jar ./cookie-jar.txt $RINGZER0_URL/login)
+LOGIN_POST="curl -s -cookie cookie-jar.txt -d $LOGIN_FORM -X POST $RINGZER0_URL/login"
 LOGIN_POST=${LOGIN_POST##*<form role=\"form\" action=\"\" method=\"post\">}
 LOGIN_POST=${LOGIN_POST%%</form>*}
 echo $LOGIN_POST
