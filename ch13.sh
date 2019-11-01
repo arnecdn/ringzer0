5ac7e00b2621a5aad9741d1d95c45ae522e58b2d
Arne Nilsen <arne.nilsen@skatteetaten.no>
2019-11-01 09:23:29 +0100

    Adjusted parsing of hash

--- ch13.sh
+++ ch13.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 usage()
 {
   echo "Usage: $0 [-s <PHPSESSID>]"
@@ -17,8 +16,9 @@ done
 RINGZER0_URL="https://ringzer0ctf.com"
 
 GET_CHALLENGE=$(curl -sb PHPSESSID=$SESSIONID $RINGZER0_URL/challenges/13)
-#GET_CHALLENGE=${GET_CHALLENGE##*----- BEGIN MESSAGE -----}
-#GET_CHALLENGE=${GET_CHALLENGE%%<br />\r ---*}
+GET_CHALLENGE=$(echo $GET_CHALLENGE| sed -e 's/\r//g' -e 's/<br \/>//g')
+GET_CHALLENGE=${GET_CHALLENGE##*----- BEGIN MESSAGE -----}
+GET_CHALLENGE=${GET_CHALLENGE%%----- END MESSAGE -----*}
 
 echo $GET_CHALLENGE
 #echo $response | sed -n '/<body>/,/<\/body>/{ /body>/d; p }'
