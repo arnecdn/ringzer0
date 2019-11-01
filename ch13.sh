21e48d9d78f05b5b216880aa24da6982234eeab4
user user <user@workspacea6qbn1uuttz01f20.python-6d4bb6959-vntnm>
2019-11-01 21:52:54 +0000

    Completed challenge

--- ch13.sh
+++ ch13.sh
@@ -17,9 +17,17 @@ RINGZER0_URL="https://ringzer0ctf.com"
 
 GET_CHALLENGE=$(curl -sb PHPSESSID=$SESSIONID $RINGZER0_URL/challenges/13)
 GET_CHALLENGE=$(echo $GET_CHALLENGE| sed -e 's/\r//g' -e 's/<br \/>//g')
-GET_CHALLENGE=${GET_CHALLENGE##*----- BEGIN MESSAGE -----}
-GET_CHALLENGE=${GET_CHALLENGE%%----- END MESSAGE -----*}
+GET_CHALLENGE=${GET_CHALLENGE##*----- BEGIN MESSAGE ----- }
+GET_CHALLENGE=${GET_CHALLENGE%% ----- END MESSAGE -----*}
+#echo $GET_CHALLENGE
 
-echo $GET_CHALLENGE
-#echo $response | sed -n '/<body>/,/<\/body>/{ /body>/d; p }'
-#sed -n ''
+function hash() {
+    python3 - <<END
+import hashlib
+
+print(hashlib.sha512("$GET_CHALLENGE".encode('utf-8')).hexdigest())
+END
+}
+
+GET_CHALLENGE_WITH_HASH=$(curl -sb PHPSESSID=$SESSIONID $RINGZER0_URL/challenges/13/$(hash))
+echo $GET_CHALLENGE_WITH_HASH
