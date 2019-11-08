2818bc10fa2179890dc9b6151ca7f07b87245d47
Arne Nilsen <arne.nilsen@skatteetaten.no>
2019-11-08 10:41:50 +0100

    Want to compare checksu

--- ch15.sh
+++ ch15.sh
@@ -11,13 +11,28 @@ do
   esac
 done
 
-GET_CHALLENGE=$(curl -sb PHPSESSID=$SESSIONID $RINGZER0_URL_CHALLENGE)
+GENERATED_CHECKSUM="1"
+RECEIVED_CHECKSUM="0"
 
-ELF_MESSAGE=$(parse_content_between_tags "$GET_CHALLENGE" "----- BEGIN Elf Message -----" "----- End Elf Message -----")
-DECODED_ELF_MESSAGE=$(echo $ELF_MESSAGE | base64 -d) 
-echo $DECODED_ELF_MESSAGE
+#while [[ $GENERATED_CHECKSUM != $RECEIVED_CHECKSUM ]]; do
+  GET_CHALLENGE=$(curl -sb PHPSESSID=$SESSIONID $RINGZER0_URL_CHALLENGE)
 
-ELF_CHECKSUM=$(parse_content_between_tags "$GET_CHALLENGE" "---- BEGIN Checksum -----" "----- END Checksum -----")
+  RECEIVED_CHECKSUM=$(parse_content_between_tags "$GET_CHALLENGE" "---- BEGIN Checksum -----" "----- END Checksum -----")
+  echo "Mottatt sjekksum:  $RECEIVED_CHECKSUM"  
+
+  ELF_MESSAGE=$(parse_content_between_tags "$GET_CHALLENGE" "----- BEGIN Elf Message -----" "----- End Elf Message -----")
+  DECODED_ELF_MESSAGE=$(echo $ELF_MESSAGE | base64 -d) 
+
+  GENERATED_CHECKSUM=$(echo $DECODED_ELF_MESSAGE | openssl dgst -md5 -hex | sed -e 's/(stdin)=//g' -e 's/ //g')
+  echo "Generert sjekksum: $GENERATED_CHECKSUM"
+  
+#done
+
+if [[ $GENERATED_CHECKSUM = $RECEIVED_CHECKSUM ]]; then
+  echo "Riktig shekksum:   $RECEIVED_CHECKSUM"
+fi
+
+
 #echo $ELF_CHECKSUM
 
 #CHALENGE_FLAG=$(parse_flag "$GET_CHALLENGE_WITH_CRACKED_HASH")
